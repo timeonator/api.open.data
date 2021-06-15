e223b1a4e8e4a8e13d7cee7d65b7e06038e61d8b
timeonator <timeonator@gmail.com>
2021-06-15 08:09:35 -0700

    prototyped

--- server.js
+++ server.js
@@ -4,10 +4,30 @@ const app = express()
 const HOST='localhost'
 const PORT=8000
 
+DATA=  [
+    {
+        "id" : "1", 
+        "name" : "datapackage-test-1",
+        "title" : "Datapackage Test One",
+        "licenses" : [],
+        "sources" : [],
+        "resources": [],
+    }
+]
+function getDataPackage(name) {
+    return(DATA[0]);
+}
+
 app.listen(PORT, ()=>{
     console.log('listening on port ', PORT)
 })
 
 app.get('/', (req,resp) => {
     resp.send('open dataset: Hello')
+})
+
+app.get('/datapackage/:name',(req,res) => {
+    console.log(req)
+    var dp = getDataPackage(req.params.name)
+    res.json(dp)
 })
